59f591035f72e730e23f3582eba902be01c83326
tolunardahanli <38496067+tolunardahanli@users.noreply.github.com>
2021-06-29 04:03:11 +0200

    deployment ready to roll in

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t tolunardahanli/multi-client:latest -t tolunardahanli/multi-client:$SHA -f ./client/Dockerfile ./client
+docket build -t tolunardahanli/multi-server:latest -t tolunardahanli/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t tolunardahanli/multi-worker:latest -t tolunardahanli/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push tolunardahanli/multi-client:latest
+docker push tolunardahanli/multi-server:latest
+docker push tolunardahanli/multi-worker:latest
+docker push tolunardahanli/multi-client:$SHA
+docker push tolunardahanli/multi-server:$SHA
+docker push tolunardahanli/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=tolunardahanli/multi-server:$SHA
+kubectl set image deployments/client-deployment client=tolunardahanli/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=tolunardahanli/multi-worker:$SHA
